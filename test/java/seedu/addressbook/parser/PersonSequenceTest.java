404cc33b3b80456b3762de2d1a79b5bf4cf600df
Wang Leng Tan <tanwangleng@outlook.com>
2016-09-03 11:23:32 +0800

    Add 'adam' to test case

---
+++ test/java/seedu/addressbook/parser/PersonSequenceTest.java
@@ -0,0 +1,33 @@
+package seedu.addressbook.parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Address;
+import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.Phone;
+
+public class PersonSequenceTest {
+
+    /**
+     * Follows everything stated in LO-ClassLevel. (hence the name of the
+     * method)
+     * 
+     * @throws IllegalValueException
+     */
+    @Test
+    public void testLoClassLevel() throws IllegalValueException {
+        AddressBook addressBook = new AddressBook();
+
+        Person adam = new Person(new Name("Adam"), new Phone("555", false), new Email("a@a.com", false),
+                new Address("Blk A", false), null);
+        addressBook.addPerson(adam);
+        assertEquals(1, adam.getSequenceNumber());
+    }
+
+}
