b45c5152b564d9fe920feaf343e363cf1e243438
Wang Leng Tan <tanwangleng@outlook.com>
2016-09-03 11:28:12 +0800

    Add 'ben' and 'charlie' to test case

--- test/java/seedu/addressbook/parser/PersonSequenceTest.java
+++ test/java/seedu/addressbook/parser/PersonSequenceTest.java
@@ -25,10 +25,20 @@ public class PersonSequenceTest {
     public void testLoClassLevel() throws IllegalValueException {
         AddressBook addressBook = new AddressBook();
 
-        Person adam = new Person(new Name("Adam"), new Phone("555", false), new Email("a@a.com", false),
+        Person adam = new Person(new Name("Adam"), new Phone("111", false), new Email("a@a.com", false),
                 new Address("Blk A", false), new UniqueTagList());
         addressBook.addPerson(adam);
         assertEquals(1, adam.getSequenceNumber());
+        
+        Person ben = new Person(new Name("Ben"), new Phone("222", false), new Email("b@b.com", false),
+                new Address("Blk B", false), new UniqueTagList());
+        addressBook.addPerson(ben);
+        assertEquals(2, ben.getSequenceNumber());
+                
+        Person charlie = new Person(new Name("Charlie"), new Phone("333", false), new Email("c@c.com", false),
+                new Address("Blk C", false), new UniqueTagList());
+        addressBook.addPerson(charlie);
+        assertEquals(3, charlie.getSequenceNumber());
     }
 
 }
