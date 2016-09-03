8870132a5d667e8176f817e948e22e1c010523aa
Wang Leng Tan <tanwangleng@outlook.com>
2016-09-03 12:28:03 +0800

    Add 'Printable' interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Allow classes to declare and expose their 
+ * printing functionality for human readable
+ * strings.
+ * 
+ * @author Tan Wang Leng
+ */
+public interface Printable {
+    /**
+     * @return some nicely formatted string
+     */
+    public String getPrintableString();
+}
